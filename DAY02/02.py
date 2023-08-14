e7f01f8fabe2d95e1dbb75cbc2e4fad5e52cf55e
Raksha Pahariya <100781382+RP2025@users.noreply.github.com>
2023-08-14 23:06:08 +0530

    Create 02.py

---
+++ DAY02/02.py
@@ -0,0 +1,9 @@
+from typing import *
+
+def compareIfElse(a: int, b: int):
+    # Write your code here
+    if a == b:
+        return "equal"
+    if a > b:
+        return "greater"
+    return "smaller"
